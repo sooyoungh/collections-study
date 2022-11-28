86c80ca467e467575818d5590a0d69297266efe5
sooyoungh <sooyoung.h8@gmail.com>
2022-11-28 22:24:45 +0900

    ✅ test(SetCollections): Set 컬렉션의 삽입 연산 성능 비교 기능 구현

---
+++ src/jmh/java/com/SetCollections.java
@@ -0,0 +1,48 @@
+package com;
+
+import org.openjdk.jmh.annotations.*;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class SetCollections {
+
+    public Integer COUNT = 100_000;
+    public Random random = new Random();
+
+    public Set<Integer> hashSet;
+    public Set<Integer> treeSet;
+    public Set<Integer> linkedHashSet;
+
+    @Benchmark
+    public void addHashSet() {
+
+        hashSet = new HashSet<>();
+        for (int i = 0; i < COUNT; i++) {
+            Integer randomNumber = random.nextInt();
+            hashSet.add(randomNumber);
+        }
+    }
+
+    @Benchmark
+    public void addTreeSet() {
+
+        treeSet = new TreeSet<>();
+        for (int i = 0; i < COUNT; i++) {
+            Integer randomNumber = random.nextInt();
+            treeSet.add(randomNumber);
+        }
+    }
+
+    @Benchmark
+    public void addLinkedHashSet() {
+
+        linkedHashSet = new LinkedHashSet<>();
+        for (int i = 0; i < COUNT; i++) {
+            Integer randomNumber = random.nextInt();
+            linkedHashSet.add(randomNumber);
+        }
+    }
+}
